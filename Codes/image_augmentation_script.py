67dea4b3a75521cd57d51bc864f2202180c43cf7
Shidqi Akram Hauzan <57066616+Wafelkertas@users.noreply.github.com>
2021-05-25 12:56:33 +0700

    Updated Jupyter Notebook script
    Updated Jupyter Notebook script

---
+++ Codes/image_augmentation_script.py
@@ -0,0 +1,58 @@
+import os
+import random
+from scipy import ndarray
+
+
+import skimage as sk
+from skimage import transform
+from skimage import util
+from skimage import io
+
+def random_rotation(image_array: ndarray):
+    
+    random_degree = random.uniform(-25, 25)
+    return sk.transform.rotate(image_array, random_degree)
+
+def random_noise(image_array: ndarray):
+    
+    return sk.util.random_noise(image_array)
+
+def horizontal_flip(image_array: ndarray):
+    
+    return image_array[:, ::-1]
+
+
+available_transformations = {
+    'rotate': random_rotation,
+    'noise': random_noise,
+    'horizontal_flip': horizontal_flip
+}
+
+folder_path = '../images/Poisonous'
+# folder_path = input("Select your file path ")
+num_files_desired = 1000
+
+images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+
+num_generated_files = 0
+while num_generated_files <= num_files_desired:
+  
+    image_path = random.choice(images)
+    
+    image_to_transform = sk.io.imread(image_path)
+   
+    num_transformations_to_apply = random.randint(1, len(available_transformations))
+
+    num_transformations = 0
+    transformed_image = None
+    while num_transformations <= num_transformations_to_apply:
+        
+        key = random.choice(list(available_transformations))
+        transformed_image = available_transformations[key](image_to_transform)
+        num_transformations += 1
+
+        new_file_path = '%s/augmented_image_%s.png' % (folder_path, num_generated_files)
+
+        
+        io.imsave(new_file_path, transformed_image)
+    num_generated_files += 1
